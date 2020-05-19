33166942718672d92df7b7f9b19ce14efc2b61cb
Luis Guillermo Natera Orozco <nateraluis@gmail.com>
2020-05-19 11:58:10 +0200

    docstrings

--- src/visualization.py
+++ src/visualization.py
@@ -1,10 +1,28 @@
 import matplotlib.pyplot as plt
 import geopandas as gpd
 
-def hex_plot(gdf_hex, gdf_data, gdf_boundary, gdf_edges, save_png=False, save_pdf=False,show=False, name=plot,dpi=300,transparent=True):
+def hex_plot(gdf_hex, gdf_data, gdf_boundary, gdf_edges, column ,save_png=False, save_pdf=False,show=False, name=plot,dpi=300,transparent=True):
+	"""
+	Plot hexbin geoDataFrames to create the accesibility plots.
+
+	Arguments:
+		gdf_hex {geopandas.GeoDataFrame} -- Original geoDataFrame with the hexbins
+		gdf_data {geopandas.GeoDataFrame} -- geoDataFrame with the data to be plotted
+		gdf_boundary {geopandas.GeoDataFrame} -- geoDataFrame with the boundary to use 
+		gdf_edges {geopandas.GeoDataFrame} -- geoDataFrame with the edges (streets)
+		column {geopandas.GeoDataFrame} -- column to plot from the gdf_data geoDataFrame
+
+	Keyword Arguments:
+		save_png {bool} -- save the plot in png or not (default: {False})
+		save_pdf {bool} -- save the plot in pdf or not (default: {False})
+		show {bool} -- show the plot or not (default: {False})
+		name {str} -- name for the plot to be saved if save=True (default: {plot})
+		dpi {int} -- resolution to use (default: {300})
+		transparent {bool} -- save with transparency or not (default: {True})
+	"""
 	fig, ax = plt.subplots(1,1,figsize=(15,15))
 	gdf_hex.plot(ax=ax,color='#2b2b2b', alpha=0.95, linewidth=0.1, edgecolor='k', zorder=0)
-	gdf_data.plot(ax=ax,column='dist', cmap='magma_r',vmax=1000,zorder=1,legend=True)
+	gdf_data.plot(ax=ax,column=column, cmap='magma_r',vmax=1000,zorder=1,legend=True)
 	gdf_boundary.boundary.plot(ax=ax,color='#f8f8f8',zorder=2,linestyle='--',linewidth=0.5)
 	gdf_edges[(gdf_edges['highway']=='motorway') | (gdf_edges['highway']=='motorway_link')].plot(ax=ax,color='#898989',alpha=0.85,linewidth=2.5,zorder=3)
 	gdf_edges[(gdf_edges['highway']=='primary') | (gdf_edges['highway']=='primary_link')].plot(ax=ax,color='#898989',alpha=0.85,linewidth=1.5,zorder=3)
