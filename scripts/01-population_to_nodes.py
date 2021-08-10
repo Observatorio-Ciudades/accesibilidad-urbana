8cf7bdcf333a1e493ca2803fd9e87499cf8f7204
edgaregurrola <edgaregurrola@hotmail.com>
2021-08-10 15:31:06 +0000

    Updates search method for nodes

--- scripts/01-population_to_nodes.py
+++ scripts/01-population_to_nodes.py
@@ -15,9 +15,6 @@ def main(schema, folder_sufix, year, column_start, column_end, resolution=8, sav
     df = pd.read_json("Metropolis_CVE.json")
     aup.log("Read metropolitan areas and capitals json")
     
-    # Download municipality polygons from database
-    #gdf = aup.gdf_from_db("mpos_"+year, "marco")
-    #aup.log(f"Finished downloading mpos_{year} GeoDataFrame from database")
     #Folder names from database
     mpos_folder = 'mpos_'+year
     censo_folder = 'censoageb_' + year
@@ -45,28 +42,34 @@ def main(schema, folder_sufix, year, column_start, column_end, resolution=8, sav
             query = f"SELECT * FROM hexgrid.hex_grid WHERE \"CVEGEO\" LIKE \'{m}%%\'"
             hex_bins = hex_bins.append(aup.gdf_from_query(query, geometry_col='geometry'))
             aup.log(f"Donwloaded hex bins for {m}")
-
+            
         #Define projections
         mun_gdf = mun_gdf.set_crs("EPSG:4326")
         ageb_gdf = ageb_gdf.set_crs("EPSG:4326")
         hex_bins = hex_bins.set_crs("EPSG:4326")
 
+        ##############################################################
         # Reads mun_gdf GeoDataFrame as polygon
-        poly = mun_gdf.geometry
+        #poly = mun_gdf.geometry
         # Extracts coordinates from polygon as DataFrame
-        coord_val = poly.bounds
+        #coord_val = poly.bounds
         # Gets coordinates for bounding box
-        n = coord_val.maxy.max()
-        s = coord_val.miny.min()
-        e = coord_val.maxx.max()
-        w = coord_val.minx.min()
-        aup.log(
-            f"Extracted min and max coordinates from the municipality\
-            polygon N:{round(n,5)} S:{round(s,5)} E:{round(e,5)} W:{round(w,5)}"
-        )
+        #n = coord_val.maxy.max()
+        #s = coord_val.miny.min()
+        #e = coord_val.maxx.max()
+        #w = coord_val.minx.min()
+        #aup.log(
+        #    f"Extracted min and max coordinates from the municipality\
+        #    polygon N:{round(n,5)} S:{round(s,5)} E:{round(e,5)} W:{round(w,5)}"
+        #)
+
+        #query = f"SELECT * FROM osmnx_new.nodes WHERE (\"x\" between \'{w}\' and \'{e}\') and (\"y\" between \'{s}\' and \'{n}\')"
+        ##########################################
 
         # Creates query to download nodes from the metropolitan area or capital
-        query = f"SELECT * FROM osmnx_new.nodes WHERE (\"x\" between \'{w}\' and \'{e}\') and (\"y\" between \'{s}\' and \'{n}\')"
+        poly_wkt = mun_gdf.dissolve().geometry.to_wkt()[0]
+        aup.log("Created wkt based on dissolved polygon")
+        query = f"SELECT * FROM osmnx_new.nodes WHERE ST_Intersects(geometry, \'SRID=4326;{poly_wkt}\')"
         nodes = aup.gdf_from_query(query, geometry_col='geometry')
         aup.log(f"Downloaded {len(nodes)} nodes from database for {c}")
         # Adds population data to nodes
