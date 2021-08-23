27125e5e7aebef3b8da3c0b912f74165bde1afc0
edgaregurrola <edgaregurrola@hotmail.com>
2021-08-23 16:18:20 +0000

    First steps for the accessibility index

---
+++ scripts/04-accessibility-index.py
@@ -0,0 +1,12 @@
+import os
+import sys
+
+import pandas as pd
+import geopandas as gpd
+
+module_path = os.path.abspath(os.path.join('..'))
+if module_path not in sys.path:
+    sys.path.append(module_path)
+    import aup
+
+
