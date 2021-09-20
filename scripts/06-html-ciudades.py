c5b92f7db489be3ae9d6055914aa806b2305b426
Luis Natera <natera@hey.com>
2021-09-20 18:47:05 +0200

    Fix kepler
    If the column to display is 'idx_accessibility' round it with two
    decimals.

--- scripts/06-html-ciudades.py
+++ scripts/06-html-ciudades.py
@@ -32,6 +32,8 @@ def load_data(df, c):
 
 def make_html(config, hex_bins, column, c):
     hex_bins[column] = hex_bins[column].astype(float)
+    if column == 'idx_accessibility':
+        hex_bins[column] = hex_bins[column].round(2)
     config["config"]["visState"]["layers"][0]["visualChannels"]["colorField"][
         "name"
     ] = column
