b04ddae3a647807866190e3fe848f9df43be9533
sfajardot <86973930+sfajardot@users.noreply.github.com>
2022-03-17 17:10:13 -0600

    popnodes_update_function

--- scripts/03-population_to_nodes.py
+++ scripts/03-population_to_nodes.py
@@ -12,15 +12,15 @@ if module_path not in sys.path:
 
 def main(schema, folder_sufix, year, column_start, column_end, resolution=8, save=False):
     # Read json with municipality codes by capital or metropolitan area
-    df = pd.read_json("Metropolis_CVE.json")
+    df = pd.read_json("/home/jovyan/work/scripts/Metropolis_CVE.json")
     aup.log("Read metropolitan areas and capitals json")
     
     #Folder names from database
     mpos_folder = 'mpos_'+year
     censo_folder = 'censoageb_' + year
-    
     # Iterate over municipality DataFrame columns to access each municipality code
     for c in df.columns.unique():
+
         aup.log(f"\n Starting municipality filters for {c}")
         # Creates empty GeoDataFrame to store specified municipality polygons
         mun_gdf = gpd.GeoDataFrame()
@@ -50,53 +50,60 @@ def main(schema, folder_sufix, year, column_start, column_end, resolution=8, sav
         hex_bins = hex_bins.set_crs("EPSG:4326")
 
         #Download nodes with distance to denue data
-        _, nodes, _ = aup.graph_from_hippo(mun_gdf, 'osmnx')
+        if year == '2010':
+            _, nodes, _ = aup.graph_from_hippo(mun_gdf, 'networks', edges_folder='edges_2011', 
+            nodes_folder='nodes_2011')
+        elif year == '2020':
+            _, nodes, _ = aup.graph_from_hippo(mun_gdf, 'osmnx')
         aup.log(f"Downloaded {len(nodes)} nodes from database for {c}")
+
         nodes = nodes.to_crs("EPSG:4326")
 
-        avg_column = [
-            "prom_hnv",
-            "graproes",
-            "graproes_f",
-            "graproes_m",
-            "prom_ocup",
-            "pro_ocup_c",
-        ]  # columns that won't be divided by nodes
-        
+        ### VIALIDADES 2011######
+        if year == '2010':
+            nodes.drop(['ID', 'TIPOVIA', 'TIPO', 
+            'NUMERO', 'DERE_TRAN', 'ADMINISTRA', 'NUME_CARR', 'CONDICION', 
+            'ORIGEN', 'CALI_REPR', 'CVEGEO', 'NOMVIAL', 'SENTIDO', 'LONGITUD', 'UNIDAD', 
+            'vertex_pos', 'vertex_ind', 'vertex_par', 'vertex_p_1', 
+            'distance', 'angle'], inplace = True, axis=1)
+        ##########
+
+        # columns that won't be divided by nodes
+        avg_column = ["prom_hnv", "graproes", "graproes_f", 
+        "graproes_m", "prom_ocup", "pro_ocup_c"]
         # Adds population data to nodes
-        nodes_pop = aup.population_to_nodes(nodes, ageb_gdf, column_start=column_start, 
+        nodes_pop = aup.socio_polygon_to_points(nodes, ageb_gdf, column_start=column_start, 
         column_end=column_end-1, cve_column='cve_geo', avg_column=avg_column)
 
         aup.log(f"Added a total of {nodes_pop.pobtot.sum()} persons to nodes")
 
-        #Adds census data from nodes to hex bins
-        hex_temp = gpd.sjoin(nodes_pop, hex_bins) #joins nodes en hex bins
+        #Adds census data from nodes to hex bin
 
-        #convert data types
-        string_columns = ['cve_geo','cve_ent','cve_mun','cve_loc','cve_ageb',
-        'entidad','nom_ent','mun','nom_mun','loc','nom_loc','ageb',
-        'mza','cve_geo_ageb','hex_id_8','CVEGEO']
+        #### CENSO 2020 ######
+        if year == '2020':
+            string_columns = ['cve_geo','cve_ent','cve_mun','cve_loc','cve_ageb',
+            'entidad','nom_ent','mun','nom_mun','loc','nom_loc','ageb',
+            'mza','cve_geo_ageb','hex_id_8', 'x', 'y', 'street_count']
+        ###### CENSO 2010#############
+        if year == '2010':
+            string_columns = [
+            'censo', 'cve_ent', 'nom_ent', 'cve_mun', 'nom_mun',
+            'cve_loc', 'cve_ageb', 'cve_cd',
+            'hex_id_8','x', 'y', 'codigo', 'cve_geo', 'geog', 
+            'fecha_act', 'geom', 'institut', 'OID']
 
-        hex_temp = aup.convert_type(hex_temp, string_column=string_columns)
+        wgt_dict = {'prom_hnv':'pobtot', 'graproes':'pobtot',
+        'graproes_f':'pobfem', 'graproes_m':'pobmas',
+        'prom_ocup':'pobtot', 'pro_ocup_c':'pobtot'}
 
-        # grouping data
-        sum_columns = list(hex_temp.columns[column_start:column_end])
-        sum_columns = [x for x in sum_columns if x not in avg_column]
-        #sum_columns.remove("geometry")
-        agg_dict = {x: ["sum"] for x in sum_columns}
-        agg_dict.update({y: ["mean"] for y in avg_column})
+        hex_pop = aup.socio_points_to_polygon(hex_bins, nodes_pop, 
+        'hex_id_8', string_columns, wgt_dict=wgt_dict, avg_column=avg_column)
 
-        hex_temp = hex_temp.groupby(f"hex_id_{resolution}").agg(agg_dict)  # group hex bins
-        hex_temp.columns = hex_temp.columns.droplevel(1)
+        hex_upload = hex_bins.merge(hex_pop, on= 'hex_id_8')
 
-        hex_temp = hex_temp[ageb_gdf.iloc[:, column_start:column_end].columns.to_list()]  # keeps only census columns
-        hex_bins = pd.merge(hex_bins, hex_temp, right_index=True, 
-        left_on=f"hex_id_{resolution}", how="left").fillna(0)  # merges census data to original hex bins
-
-        aup.log(f"Added census data to a total of {len(hex_bins)} hex bins and {hex_bins.pobtot.sum()} population")
-
+        #aup.log(f"Added census data to a total of {len(hex_pop)} hex bins and {hex_pop.pobtot.sum()} population")
         if save:
-            aup.gdf_to_db_slow(hex_bins, "hex_bins_"+folder_sufix, schema=schema, if_exists="append")
+            aup.gdf_to_db_slow(hex_upload, "hex_bins_"+folder_sufix, schema=schema, if_exists="append")
             c_nodes = len(nodes_pop) / 10000
             aup.log(f"There are a total of {round(c_nodes,2)} nodes divisions")
             for cont in range(int(c_nodes)+1):
@@ -109,9 +116,9 @@ def main(schema, folder_sufix, year, column_start, column_end, resolution=8, sav
 if __name__ == "__main__":
     aup.log('--'*10)
     aup.log('Starting script')
-    censo_column_start = 14 #column where numeric data starts in censo
+    censo_column_start = 16 #column where numeric data starts in censo
     censo_column_end = -1 #column where numeric data ends in censo
-    year = '2020'
+    year = '2010'
     schema = 'censo'
-    folder_sufix = 'pop' #sufix for folder name
+    folder_sufix = f'pop_{year}' #sufix for folder name
     main(schema, folder_sufix, year, censo_column_start, censo_column_end, save=True)
