c199a8ca0fbb85c92a86818d33fc5745c51786da
sfajardot <86973930+sfajardot@users.noreply.github.com>
2022-05-30 10:12:18 -0500

    update distance amenities

--- scripts/02-distance_amenities.py
+++ scripts/02-distance_amenities.py
@@ -12,7 +12,7 @@ if module_path not in sys.path:
 
 def main(schema, folder_sufix, year, amenities, resolution=8, save=False):
     # Read json with municipality codes by capital or metropolitan area
-    df = pd.read_json("Metropolis_CVE.json")
+    df = pd.read_json("/home/jovyan/work/scripts/areas.json")
     aup.log("Read metropolitan areas and capitals json")
     
     #Folder names from database
@@ -71,29 +71,37 @@ def main(schema, folder_sufix, year, amenities, resolution=8, save=False):
                 query = f"SELECT * FROM denue_nodes.denue_node_2020 WHERE (ST_Intersects(geometry, \'SRID=4326;{poly_wkt}\')) AND (\"codigo_act\" = {cod})"
                 denue_amenity = denue_amenity.append(aup.gdf_from_query(query, geometry_col='geometry'))
             aup.log(f"Downloaded accumulated total of {len(denue_amenity)} {a} from database for {c}")
-            # Temporary gdfs are created based on nodes in order to add relevant data
-            df_temp = nodes.copy()
             nodes_distance = nodes.copy()
-            #Due to memory constraints, the total number of POIs will be divided in groups of 100
-            #These will run with the calculate nearest distance poi function by group and will be stored
-            # to check later
-            c_denue = len(denue_amenity)/100
-            for k in range(int(c_denue)+1):
-                aup.log(f"Starting range k = {k} of {int(c_denue)}")
-                denue_process = denue_amenity.iloc[int(100*k):int(100*(1+k))].copy()
-                nodes_distance_prep = aup.calculate_distance_nearest_poi(denue_process, nodes_analysis, 
-                edges, a, 'osmid', wght='time_min')
-                #A middle gdf is created whose columns will be the name of the amenity and the group number it belongs to
-                df_int = pd.DataFrame()
-                df_int['time_'+str(k)+a] = nodes_distance_prep['dist_'+a]
-                #The middle gdf is merged into the previously created temporary gdf to store the data
-                df_temp = df_temp.merge(df_int, left_index=True, right_index=True)
-            aup.log(f"finished")
-            #Once all groups of 100 are run, we drop the non-distance values from the temporary gdf
-            df_temp.drop(['x', 'y', 'street_count','geometry'], inplace = True, axis=1)
-            #We apply the min function to find the minimum value. This value is sent to a new df_min
-            df_min = pd.DataFrame()
-            df_min['time_'+a] = df_temp.min(axis=1)
+
+
+            ##Added this if in case there are no amenities of the type in the city. Prevents it from 
+            #Crashing if len = 0
+            if len(denue_amenity) == 0:
+                df_min = 0
+                aup.log(f"0 f{cod} found in {c}")
+            else:
+                #Due to memory constraints, the total number of POIs will be divided in groups of 100
+                #These will run with the calculate nearest distance poi function by group and will be stored
+                # to check later
+                c_denue = len(denue_amenity)/500
+                for k in range(int(c_denue)+1):
+                    aup.log(f"Starting range k = {k} of {int(c_denue)}")
+                    denue_process = denue_amenity.iloc[int(500*k):int(500*(1+k))].copy()
+                    nodes_distance_prep = aup.calculate_distance_nearest_poi(denue_process, nodes_analysis, 
+                    edges, a, 'osmid', wght='time_min')
+                    #A middle gdf is created whose columns will be the name of the amenity and the group number it belongs to
+                    df_int = pd.DataFrame()
+                    df_int['time_'+str(k)+a] = nodes_distance_prep['dist_'+a]
+                    #The middle gdf is merged into the previously created temporary gdf to store the data
+                    df_temp = df_temp.merge(df_int, left_index=True, right_index=True)
+                aup.log(f"finished")
+                #Once all groups of 100 are run, we drop the non-distance values from the temporary gdf
+                df_temp.drop(['x', 'y', 'street_count','geometry'], inplace = True, axis=1)
+                #We apply the min function to find the minimum value. This value is sent to a new df_min
+                df_min = pd.DataFrame()
+                df_min['time_'+a] = df_temp.min(axis=1)
+
+
             #We merge df_min which contains the shortest distance to the POI with nodes_distance which will store
             #all final data
             nodes_distance = nodes_distance.merge(df_min, left_index=True, right_index=True)
@@ -126,8 +134,22 @@ def main(schema, folder_sufix, year, amenities, resolution=8, save=False):
             hex_bins = hex_bins.set_crs("EPSG:4326")
             nodes_amenities = nodes_amenities.set_crs("EPSG:4326")
 
+            #Give more efficient format to table
+            
+            for idx,row in hex_bins.iterrows():
+                data = {'hex_id_8': ['id'],
+                'time': ['time'], 'amenity':['amenity']}
+                hex_data = pd.DataFrame(data)
+                hex_id = hex_bins.loc[idx, 'hex_id_8']
+                time = hex_bins.loc[idx, 'time_'+a]
+                amenity = a
+                hex_data['hex_id_8']= hex_id
+                hex_data['time']= time
+                hex_data['amenity']= str(amenity)
+                hex_format = hex_format.append(hex_data)
+
         if save:
-            aup.gdf_to_db_slow(hex_bins, "hex_bins_"+folder_sufix, schema=schema, if_exists="append")
+            aup.gdf_to_db_slow(hex_format, "hex_bins_"+folder_sufix, schema=schema, if_exists="append")
             #Due to memory constraints the nodes are uploaded in groups of 10,000
             c_nodes = len(nodes_amenities)/10000
             for p in range(int(c_nodes)+1):
@@ -143,8 +165,11 @@ if __name__ == "__main__":
     aup.log('--'*10)
     aup.log('Starting script')
     year = '2020'
-    schema = 'processed'
-    folder_sufix = 'time_2020' #sufix for folder name
-    amenities = {'farmacia':[464111,464112],'hospitales':[622111,622112], 
-    'supermercados':[462111,462112]}
-    main(schema, folder_sufix, year, amenities, save = False)
+    schema = 'time_amenities_clean'
+    folder_sufix = 'time_2020_clues' #sufix for folder name
+    amenities = {'denue_supermercado': [462111,462112],'denue_farmacia': [464111,464112], 
+            'denue_hospital': [622111,622112], 'denue_kinder':[611111, 611112], 'denue_primaria':[611121, 611122],
+            'denue_secundaria':[611131, 611132], 'denue_escuela_mixta': [611171, 611172], 
+            'denue_carniceria': [461121], 'denue_polleria': [461122], 'denue_pescaderia': [461123], 
+            'denue_verduleria': [41130] }
+    main(schema, folder_sufix, year, amenities, save = True)
