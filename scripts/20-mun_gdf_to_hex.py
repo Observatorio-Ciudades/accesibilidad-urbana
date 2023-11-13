efa08dac8fbf899741d7a644b2ef25118c91b315
edgaregurrola <edgaregurrola@hotmail.com>
2023-11-13 16:08:36 -0600

    Integrate municipalities with out of boundaries AGEB

--- scripts/20-mun_gdf_to_hex.py
+++ scripts/20-mun_gdf_to_hex.py
@@ -11,8 +11,27 @@ if module_path not in sys.path:
 
 def main(city, res, mun_gdf, ageb_gdf, save=False):
 
+    #############################################
+
+    # assertion errors
+    # dissolve mun_gdf and ageb_gdf
+    mun_buff = mun_gdf.to_crs("EPSG:6372").buffer(500)
+    mun_buff = mun_buff.to_crs("EPSG:4326")
+    mun_buff = gpd.GeoDataFrame(geometry=mun_buff).dissolve()
+
+    ageb_buff = ageb_gdf.to_crs("EPSG:6372").buffer(500)
+    ageb_buff = ageb_buff.to_crs("EPSG:4326")
+    ageb_buff = gpd.GeoDataFrame(geometry=ageb_buff).dissolve()
+
+    # merge gdfs
+    gdf_merge = pd.concat([ageb_buff, mun_buff])
+    gdf_merge = gdf_merge.dissolve()
+
+
+    #############################################
+
     # create hexagons grid
-    hex_gdf = aup.create_hexgrid(mun_gdf, res)
+    hex_gdf = aup.create_hexgrid(gdf_merge, res)
 
     aup.log(f'Created {len(hex_gdf)} hexagons for {city}')
 
@@ -25,9 +44,11 @@ def main(city, res, mun_gdf, ageb_gdf, save=False):
     cnt_join = hex_cnt.sjoin(mun_gdf).drop(columns='index_right')
 
     hex_merge = hex_gdf.merge(cnt_join[[f'hex_id_{res}','CVEGEO','NOMGEO','city']],
-             on=f'hex_id_{res}')
+             on=f'hex_id_{res}', how='outer')
     aup.log(f'Merged hexagons with municipalities for {city}')
     aup.log(f'Total hex_gdf: {len(hex_gdf)} and hex_merge: {len(hex_merge)}')
+
+    mun_hex_intersect = len(hex_merge.loc[hex_merge.CVEGEO.notna()])
 
     assert len(hex_gdf) == len(hex_merge), 'hex_gdf does not match hex_merge'
 
@@ -51,17 +72,51 @@ def main(city, res, mun_gdf, ageb_gdf, save=False):
     hex_merge.loc[:,'type'] = 'rural'
     hex_merge.loc[hex_merge[f'hex_id_{res}'].isin(hex_list),'type'] = 'urban'
 
-    urban_len = len(hex_merge.loc[hex_merge['type'] == 'urban'])
-    rural_len = len(hex_merge.loc[hex_merge['type'] == 'rural'])
+    # fill missing data
+    # fill CVEGEO, city and NOMGEO
+    aup.log(f'Filling missing data for {city}')
+    ageb_join.loc[:,'CVEGEO'] = ageb_join.cve_geo.str[:5]
+    ageb_join.loc[:,'city'] = city
+    ageb_join = ageb_join.drop(columns=['NOMGEO'])
+    ageb_join = ageb_join.merge(mun_gdf[['CVEGEO','NOMGEO']], on='CVEGEO')
+
+    # drop hex_id duplicates, preparing to join
+    ageb_to_merge = ageb_join.drop_duplicates(subset=f'hex_id_{res}')
+
+    aup.log('Dropped duplicates')
+
+    # update missing data
+    left_a = hex_merge.set_index(f'hex_id_{res}')
+    right_a = ageb_to_merge[[f'hex_id_{res}','CVEGEO','city','NOMGEO']].set_index(f'hex_id_{res}')
+    aup.log('Ready to update missing data')
+    hex_fill = left_a.reindex(columns=left_a.columns.union(right_a.columns))
+    hex_fill.update(right_a)
+    hex_fill.reset_index(inplace=True)
+
+    aup.log('Updated missing data')
+
+    hex_fill = hex_fill.loc[~((hex_fill.CVEGEO.isna())&(hex_fill['type']=='rural'))] 
+
+    urban_len = len(hex_fill.loc[hex_fill['type'] == 'urban'])
+    rural_len = len(hex_fill.loc[hex_fill['type'] == 'rural'])
     aup.log(f'Created {urban_len} urban and {rural_len} rural hexagons')
+    aup.log(f'Final hex_fill length: {len(hex_fill)} compaered to intersection: {mun_hex_intersect} length')
 
     if save:
         schema = 'hexgrid'
-        table = f'hexgrid_{res}_city'
+        table = f'hexgrid_{res}_city_2020'
 
-        aup.gdf_to_db_slow(hex_merge, schema, table, if_exists='append')
+        limit_len = 500000
+        if len(hex_fill)>limit_len:
+            c_upload = len(hex_fill)/limit_len
+            for k in range(int(c_upload)+1):
+                aup.log(f"Starting range k = {k} of {int(c_upload)}")
+                hex_upload = hex_fill.iloc[int(limit_len*k):int(limit_len*(1+k))].copy()
+                aup.gdf_to_db_slow(hex_upload, table, schema, if_exists='append')
+        else:
+            aup.gdf_to_db_slow(hex_fill, table, schema, if_exists='append')
 
-
+    
 if __name__ == "__main__":
     aup.log('--'*20)
     aup.log('Starting script')
@@ -77,10 +132,10 @@ if __name__ == "__main__":
 
     aup.log(f"Downloaded city names for {len(city_names)} cities")
 
-    save = False
+    save = True
 
-    for city in city_names.city.unique():
-
+    # for city in city_names.city.unique():
+    for city in city_names:
         # download municipality polygons
         schema = 'metropolis'
         table = 'metro_gdf_2020'
