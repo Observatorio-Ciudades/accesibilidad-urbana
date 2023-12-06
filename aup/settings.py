16553eee7c45fd791a5477ab169a8aa0c0e28602
JAJP2203 <jajp2203@gmail.com>
2023-12-06 22:48:38 +0000

    .

--- aup/settings.py
+++ aup/settings.py
@@ -1,7 +1,7 @@
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 import os
 
-load_dotenv()
+#load_dotenv()
 
 """Global settings, can be configured by user with utils.config()."""
 
@@ -18,7 +18,7 @@ log_name = "ObsCd"
 log_filename = "ObsCd"
 
 # Database settings
-url = os.getenv('url')
-user = os.getenv('user')
-pw = os.getenv('pw')
-db = os.getenv('db')
+url = 'hippocampus.cswst4rid7eb.us-east-2.rds.amazonaws.com'
+user = 'odc_writer'
+pw = 'writejacobs1918'
+db = 'postgres'
