2a84a09b20385b2641251fcd8706bb6a85435c24
Alex Loya <a.loyamr@gmail.com>
2024-02-26 23:35:56 +0000

    basedata_slope_to_edges_script_wip

--- scripts/08-slope-to-edges.py
+++ scripts/08-slope-to-edges.py
@@ -12,33 +12,21 @@ if module_path not in sys.path:
     import aup
 
 
-def main(schema, folder_sufix, year, grl_path, save=False):
-
-    # Read json with municipality codes by capital or metropolitan area
-    df = pd.read_json("Metropolis_CVE.json")
-    aup.log("Read metropolitan areas and capitals json")
-
-    # Iterate over municipality DataFrame columns to access each municipality code
-    for c in df.columns.unique():
-        aup.log(f"\n Starting municipality filters for {c}")
-        # Creates empty GeoDataFrame to store specified municipality polygons and hex grid
-        mun_gdf = gpd.GeoDataFrame()
-        # Iterates over municipality codes for each metropolitan area or capital
-        query = f"SELECT * FROM metropolis.metro_list WHERE \"city\" LIKE \'{c}\'"
-        mun_gdf = aup.gdf_from_query(query, geometry_col='geometry')
-
-        #Define projections for municipalities and hexgrids
-        mun_gdf = mun_gdf.set_crs("EPSG:4326")
+def main(mun_gdf, save=False):
 
         # Creates query to download OSMNX nodes and edges from the DB
         # by metropolitan area or capital using the municipality geometry
-        G, nodes, edges = aup.graph_from_hippo(mun_gdf, 'osmnx')
-        aup.log(f"Downloaded {len(nodes)} nodes and {len(edges)} edges from database for {c}")
+        G, nodes, edges = aup.graph_from_hippo(mun_gdf, schema, edges_table, nodes_table)
+        # For 2020 only:
+        if nodes_table == 'nodes_23_point':
+            nodes = nodes.drop(columns=['city'])
+        if edges_table == 'edges_23_line':
+            edges = edges.drop(columns=['city'])
+        aup.log(f"--- Downloaded {len(nodes)} nodes and {len(edges)} edges from database for {city}.")
 
         mde_path = [] # list to append mde path strings
         #Gathers state codes for MDE
-        for i in range(len(df.loc["edo", c])):
-            e = df.loc["edo", c][i]
+        for e in mun_gdf['CVE_ENT'].unique():
             tmp_path = grl_path + (f'CEM_V3_20170619_R15_E{e}_TIF/')
 
             #search for files in tmp_path for .tif
@@ -53,8 +41,8 @@ def main(schema, folder_sufix, year, grl_path, save=False):
         nodes_elev_mde, edges_elev_mde = ox.graph_to_gdfs(G_elev_mde, nodes=True, edges=True)
         mean_elev = round(nodes_elev_mde.elevation.mean(),2)
         mean_slope = round(edges_elev_mde.grade_abs.mean(),2)
-        aup.log(f"Assigned a mean elevation of {mean_elev} to nodes "+
-        f"\nand mean slope of {mean_slope} to edges")
+        aup.log(f"--- Assigned a mean elevation of {mean_elev} to nodes")
+        aup.log(f"    and mean slope of {mean_slope} to edges.")
         # reset index for upload
         nodes_elev_mde.reset_index(inplace=True)
         edges_elev_mde.reset_index(inplace=True)
@@ -65,24 +53,75 @@ def main(schema, folder_sufix, year, grl_path, save=False):
 
         #upload
         if save:
-            aup.gdf_to_db_slow(edges_elev_mde, "edges_"+folder_sufix, 
+            aup.gdf_to_db_slow(edges_elev_mde, "edges_"+folder_sufix+"_line", 
             schema=schema, if_exists="append")
+            aup.log(f"--- Uploaded edges into DB.")
             #Due to memory constraints the nodes are uploaded in groups of 10,000
             c_nodes = len(nodes_elev_mde)/10000
             for p in range(int(c_nodes)+1):
                 nodes_upload = nodes_elev_mde.iloc[int(10000*p):int(10000*(p+1))].copy()
-                aup.gdf_to_db_slow(nodes_upload, "nodes_"+folder_sufix, 
+                aup.gdf_to_db_slow(nodes_upload, "nodes_"+folder_sufix+"_point", 
                 schema=schema, if_exists="append")
-                aup.log(f"uploaded {10000*(p+1)} nodes into DB out of {len(nodes_elev_mde)}")
-            aup.log("Finished uploading nodes ")
-
-
+                aup.log(f"--- Uploaded {10000*(p+1)} nodes into DB out of {len(nodes_elev_mde)}.")
+            aup.log("--- Finished uploading nodes.")
 
 if __name__ == "__main__":
-    aup.log('--'*20)
-    aup.log('\n Starting script')
+    aup.log('--'*50)
+    aup.log('\n Starting script.')
+
+    # --------------- PARAMETERS
+
+    # City data
+    metro_schema = 'metropolis'
+    metro_table = 'metro_gdf_2020' # metro_gdf_2015 or metro_gdf_2020
+    # Network data
     schema = 'osmnx'
-    folder_sufix = 'elevation' #sufix for folder name
-    year = '2020'
+    nodes_table = 'nodes_23_point' # nodes or nodes_23_point
+    edges_table = 'edges_23_line' # edges or edges_23_line
+    # Location of unzipped MDE data
     grl_path = '../data/external/MDE/'
-    main(schema, folder_sufix, year, grl_path, save=True)
+    # Folder sufix
+    folder_sufix = 'elevation_24' # elevation or elevation_24
+
+    # --------------- SCRIPT
+    # Load all cities
+    aup.log("--- Reading available cities.")
+    query = f"SELECT city FROM {metro_schema}.{metro_table}"
+    metro_df = aup.df_from_query(query)
+    city_list = list(metro_df.city.unique())
+
+    #In metro_gdf_2020 CDMX was separated from the rest of ZMVM. 
+    # For current elevation analysis, it's better if they are together.
+    if metro_table == 'metro_gdf_2020':
+        city_list.remove('CDMX') 
+
+    # Iterate over municipality DataFrame columns to access each municipality code
+    for city in city_list:
+        aup.log(f"--- Loading municipalities for {city}.")
+
+        # Creates empty GeoDataFrame to store specified municipality polygons and hex grid
+        mun_gdf = gpd.GeoDataFrame()
+
+        if (metro_table =='metro_gdf_2020') and (city == 'ZMVM'):
+            # Loads ZMVM
+            city = 'ZMVM'
+            query = f"SELECT * FROM {metro_schema}.{metro_table} WHERE \"city\" LIKE \'{city}\'"
+            gdf_1 = aup.gdf_from_query(query, geometry_col='geometry')
+            # Loads CDMX
+            city = 'CDMX'
+            query = f"SELECT * FROM {metro_schema}.{metro_table} WHERE \"city\" LIKE \'{city}\'"
+            gdf_2 = aup.gdf_from_query(query, geometry_col='geometry')
+            # Concatenates both
+            mun_gdf = pd.concat([gdf_1,gdf_2])
+
+        else:
+            # Loads current city
+            query = f"SELECT * FROM {metro_schema}.{metro_table} WHERE \"city\" LIKE \'{city}\'"
+            mun_gdf = aup.gdf_from_query(query, geometry_col='geometry')
+
+        #Define projections for municipalities and hexgrids
+        mun_gdf = mun_gdf.set_crs("EPSG:4326")
+
+        # Run main function
+        aup.log(f"--- Starting main function for {city}.")
+        main(mun_gdf, save=True)
