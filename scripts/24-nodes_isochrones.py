263ceae0266c4e71ce62ab2671ad42e33befb422
edgaregurrola <edgaregurrola@hotmail.com>
2024-06-08 20:15:41 -0600

    Updated multiprocessing script

--- scripts/24-nodes_isochrones.py
+++ scripts/24-nodes_isochrones.py
@@ -11,25 +11,20 @@ module_path = os.path.abspath(os.path.join('..'))
 if module_path not in sys.path:
     sys.path.append(module_path)
     import aup
-import multiprocessing
 
-def main(trip_time, prox_measure, projected_crs, db_save=False, local_save=False):
+from multiprocessing import Pool
+
+def chunker_list(seq, size):
+    return (seq[i::size] for i in range(size))
+
+# Define a function to be executed by each process
+def create_isochrone(input_tuple):
+        G, nodes, edges, point_of_interest, trip_time, prox_measure, projected_crs = input_tuple
+        return aup.proximity_isochrone(G, nodes, edges, point_of_interest, trip_time, prox_measure, projected_crs)
+
+def main(trip_time, prox_measure, projected_crs, nodes_list, nodes, edges, G, db_save=False, local_save=False):
     aup.log("--"*40)
     aup.log(f"--- STARTING MAIN FUNCTION.")
-    
-    
-    # 1.1 --------------- BASE DATA FOR POIS-NODES ANALYSIS
-    # ------------------- This step downloads the area of interest and network used to measure distance.
-
-    # Area of interest (aoi)
-    aup.log("--- Downloading area of interest.")
-    query = f"SELECT * FROM {aoi_schema}.{aoi_table} WHERE \"city\" LIKE \'{city}\'"
-    aoi = aup.gdf_from_query(query, geometry_col='geometry')
-    aoi = aoi.set_crs("EPSG:4326")
-
-    # OSMnx Network
-    aup.log("--- Downloading network.")
-    G, nodes, edges = aup.graph_from_hippo(aoi, network_schema, edges_table, nodes_table, projected_crs)
 
 
     # 1.2 --------------- CREATE ISOCHRONES FOR ALL NODES
@@ -37,62 +32,57 @@ def main(trip_time, prox_measure, projected_crs, db_save=False, local_save=False
     
     # Create pois from nodes
     aup.log("--- Considering all nodes as pois.")
-    pois = nodes.reset_index().copy()[['osmid','geometry']]
-    osmid_list = list(pois.osmid.unique())
+    # pois = nodes.reset_index().copy()[['osmid','geometry']]
+    # osmid_list = list(pois.osmid.unique())
     '''# Test mode creates the first 100 isochrones only and saves to local only.
     if test:
         osmid_list = osmid_list[0:100]'''
 
-    k = len(osmid_list)
+    k = len(nodes_list)
 
     # Integrate multiprocessor here
     isochrones_gdf = gpd.GeoDataFrame()
 
     # Create a multiprocessing input list
-    input_list = [(G, nodes, edges, point_of_interest, trip_time, prox_measure, projected_crs) for point_of_interest in pois.itertuples()]
-
-    # Define a function to be executed by each process
-    def create_isochrone(input_tuple):
-        G, nodes, edges, point_of_interest, trip_time, prox_measure, projected_crs = input_tuple
-        return aup.proximity_isochrone(G, nodes, edges, point_of_interest, trip_time, prox_measure, projected_crs)
+    # input_list = [(G, nodes, edges, point_of_interest, trip_time, prox_measure, projected_crs) for point_of_interest in pois.itertuples()]
 
     # Create a multiprocessing pool
-    pool = multiprocessing.Pool()
+    # pool = multiprocessing.Pool()
 
     # Map the create_isochrone function to the input list using the multiprocessing pool
-    results = pool.map(create_isochrone, input_list)
+    # results = pool.map(create_isochrone, input_list)
 
     # Close the multiprocessing pool
-    pool.close()
-    pool.join()
+    # pool.close()
+    # pool.join()
 
     # Iterate over the results and assign osmid and geometry to the isochrones_gdf
-    for i, result in enumerate(results):
-        isochrones_gdf.loc[i, 'osmid'] = pois.loc[i, 'osmid']
-        isochrones_gdf.loc[i, 'geometry'] = result
+    # for i, result in enumerate(results):
+    #     isochrones_gdf.loc[i, 'osmid'] = pois.loc[i, 'osmid']
+    #     isochrones_gdf.loc[i, 'geometry'] = result
 
     # Single process version
-    '''
+    
     # Iterate over each node and create an isochrones
     aup.log("--- Iterating over all pois.")
     isochrones_gdf = gpd.GeoDataFrame()
     i = 0
-    for osmid in osmid_list:
+    for osmid in nodes_list:
 
         aup.log(f"Creating isochrone for node_poi {i}/{k}.")
 
         # Current node being analysed
-        point_of_interest = pois.loc[pois.osmid == osmid].copy()
+        # point_of_interest = pois.loc[pois.osmid == osmid].copy()
         
         # Calculate isochrone geometry
-        hull_geom = aup.proximity_isochrone(G, nodes, edges, point_of_interest, trip_time, prox_measure, projected_crs)
+        hull_geom = aup.proximity_isochrone_from_osmid(G, nodes, edges, osmid, trip_time, prox_measure, projected_crs)
 
         # Assign osmid and geometry to created isochrone and save to gdf
-        isochrones_gdf.loc[i,'osmid'] = point_of_interest.osmid.unique()[0]
+        isochrones_gdf.loc[i,'osmid'] = osmid
         isochrones_gdf.loc[i,'geometry'] = hull_geom
 
         i += 1
-    '''
+    
 
     # 1.3 --------------- SAVING
     if local_save:
@@ -141,18 +131,43 @@ if __name__ == "__main__":
     # ---------------------------- SCRIPT CONFIGURATION - SAVING ----------------------------
 
     ##### WARNING ##### WARNING ##### WARNING #####
-    db_save = False # save output to database?
-    local_save = True # save output to local? (Make sure directory exists)
+    db_save = True # save output to database?
+    local_save = False # save output to local? (Make sure directory exists)
     isochrones_local_save_dir = f"../data/processed/santiago/test_script24_nodesisochrones.gpkg"
     ##### WARNING ##### WARNING ##### WARNING #####
 
     # Test mode overrides db_save (Does not save to database), 
     # creates isochrones for the first 100 nodes only,
     # and saves to local only (Make sure directory exists).
-    test = True
+    test = False
+
+    # 1.1 --------------- BASE DATA FOR POIS-NODES ANALYSIS
+    # ------------------- This step downloads the area of interest and network used to measure distance.
+
+    # Area of interest (aoi)
+    aup.log("--- Downloading area of interest.")
+    query = f"SELECT * FROM {aoi_schema}.{aoi_table} WHERE \"city\" LIKE \'{city}\'"
+    aoi = aup.gdf_from_query(query, geometry_col='geometry')
+    aoi = aoi.set_crs("EPSG:4326")
+
+    # OSMnx Network
+    aup.log("--- Downloading network.")
+    G, nodes, edges = aup.graph_from_hippo(aoi, network_schema, edges_table, nodes_table, projected_crs)
+    aup.log("--- Network downloaded.")
 
     # ---------------------------- SCRIPT START ----------------------------
-    if test:
-        main(trip_time, prox_measure, projected_crs, db_save=False, local_save=True)
-    else:
-        main(trip_time, prox_measure, projected_crs, db_save, local_save)
+    # if test:
+    #     main(trip_time, prox_measure, projected_crs, db_save=False, local_save=True)
+    # else:
+    # split nodes for multiprocessing
+    all_nodes = list(set(list(nodes.index.unique())))
+    split_nodes = chunker_list(all_nodes, 190)
+
+    aup.log('Starting multiprocessing.')
+    # list for multiprocessing
+    
+    input_list = [(trip_time, prox_measure, projected_crs, nodes, edges, G, db_save, local_save) for input_nodes in split_nodes]
+
+    pool = Pool(processes=190)
+    pool.starmap(main, input_list)
+    pool.close()
