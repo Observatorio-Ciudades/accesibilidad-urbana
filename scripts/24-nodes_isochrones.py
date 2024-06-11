f3ad8e787d052748e1b71e449d27d79a27d4034a
edgaregurrola <edgaregurrola@hotmail.com>
2024-06-10 20:22:48 -0600

    Updated nodes isochrone script

--- scripts/24-nodes_isochrones.py
+++ scripts/24-nodes_isochrones.py
@@ -1,6 +1,6 @@
 import geopandas as gpd
 import pandas as pd
-import numpy as np
+import osmnx as ox
 
 import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
@@ -13,6 +13,11 @@ if module_path not in sys.path:
     import aup
 
 from multiprocessing import Pool
+from tqdm import tqdm
+
+import warnings
+warnings.filterwarnings("ignore")
+
 
 def chunker_list(seq, size):
     return (seq[i::size] for i in range(size))
@@ -64,25 +69,49 @@ def main(trip_time, prox_measure, projected_crs, nodes_list, nodes, edges, G, db
     # Single process version
     
     # Iterate over each node and create an isochrones
-    aup.log("--- Iterating over all pois.")
+    aup.log("--- Iterating over {k} nodes.")
     isochrones_gdf = gpd.GeoDataFrame()
-    i = 0
-    for osmid in nodes_list:
 
-        aup.log(f"Creating isochrone for node_poi {i}/{k}.")
+    # change crs
+    nodes = nodes.to_crs(projected_crs)
+    edges = edges.to_crs(projected_crs)
+
+    # i = 0
+    for i in tqdm(range(k), position=0, leave=True):
+
+        # aup.log(f"Creating isochrone for node_poi {i}/{k}.")
 
         # Current node being analysed
         # point_of_interest = pois.loc[pois.osmid == osmid].copy()
+        osmid = nodes_list[i]
         
         # Calculate isochrone geometry
-        hull_geom = aup.proximity_isochrone_from_osmid(G, nodes, edges, osmid, trip_time, prox_measure, projected_crs)
+        # hull_geom = aup.proximity_isochrone_from_osmid(G, nodes, edges, osmid, trip_time, prox_measure, projected_crs)
+        
+
+        center_node_gdf = nodes.loc[nodes.index==osmid].copy()
+        buffer = center_node_gdf.buffer(5000)
+
+        nodes_buff = gpd.clip(nodes, buffer)
+        edges_buff = gpd.clip(edges, buffer)
+        edges_buff['length'] = edges_buff.length
+
+        nodes_buff = nodes_buff.to_crs("EPSG:4326")
+        edges_buff = edges_buff.to_crs("EPSG:4326")
+        
+
+        G_buff = ox.graph_from_gdfs(nodes_buff, edges_buff)
+        # 1000m in 15 minutes at 4km/hr
+        iso_geom = aup.calculate_isochrone(G_buff, osmid, trip_time, prox_measure, True)
 
         # Assign osmid and geometry to created isochrone and save to gdf
         isochrones_gdf.loc[i,'osmid'] = osmid
-        isochrones_gdf.loc[i,'geometry'] = hull_geom
+        isochrones_gdf.loc[i,'geometry'] = iso_geom
 
-        i += 1
+        # i += 1
     
+    isochrones_gdf = isochrones_gdf.set_geometry('geometry')
+    isochrones_gdf = isochrones_gdf.set_crs("EPSG:4326")
 
     # 1.3 --------------- SAVING
     if local_save:
@@ -120,7 +149,7 @@ if __name__ == "__main__":
     # ---------------------------- SCRIPT CONFIGURATION - ANALYSIS AND OUTPUT OPTIONS ----------------------------
 
     # ANALYSIS AND OUTPUT -  Size of isochrones (minutes)
-    trip_time = 15
+    trip_time = 1000
 
     # ANALYSIS AND OUTPUT - Network distance method used in function pois_time. (If length, assumes pedestrian speed of 4km/hr.)
     prox_measure = 'length' # Must pass 'length' or 'time_min'
@@ -161,12 +190,12 @@ if __name__ == "__main__":
     # else:
     # split nodes for multiprocessing
     all_nodes = list(set(list(nodes.index.unique())))
-    split_nodes = chunker_list(all_nodes, 190)
+    split_nodes = chunker_list(all_nodes, 10)
 
     aup.log('Starting multiprocessing.')
     # list for multiprocessing
     
-    input_list = [(trip_time, prox_measure, projected_crs, nodes, edges, G, db_save, local_save) for input_nodes in split_nodes]
+    input_list = [(trip_time, prox_measure, projected_crs, input_nodes, nodes, edges, G, db_save, local_save) for input_nodes in split_nodes]
 
     pool = Pool(processes=190)
     pool.starmap(main, input_list)
