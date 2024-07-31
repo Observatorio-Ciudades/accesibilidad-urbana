2907aba1d6246ac6b4ca3a85a8d3a75f9a373201
Alex Loya <a.loyamr@gmail.com>
2024-07-31 18:50:46 +0000

    compiler_hqsl_wip02

--- scripts/27-compiler_hqsl.py
+++ scripts/27-compiler_hqsl.py
@@ -56,7 +56,7 @@ def create_filtered_navigable_network(public_space_quality_dir, projected_crs, f
     edges = gdf_exploded.copy()
     nodes = nodes.set_crs("EPSG:4326")
     edges = edges.set_crs("EPSG:4326")
-    nodes, edges = aup.create_network(nodes, edges)
+    nodes, edges = aup.create_network(nodes, edges, projected_crs)
     # Filter them
     edges_filt = edges.loc[edges[filtering_column] >= filtering_value]
 
@@ -77,12 +77,122 @@ def create_filtered_navigable_network(public_space_quality_dir, projected_crs, f
 
 
 def main(source_list, aoi, nodes, edges, G, walking_speed, local_save, save):
-    a = 1
-    return a
     
+    # 1.1 --------------- NODES PROXIMITY TO POIS
+    # ------------------- This step loads each source of interest, calculates nodes proximity and saves to database
+    k = len(source_list)-len(special_sources)
+    i = 1
 
-    
+    for source in source_list:
 
+        # ----------
+        # UNIQUE ID CONSIDERATION
+        # Check if current source has a unique ID that needs to be considered in the process
+        if source in unique_id_sources:
+            unique_id = True
+        # Check if current source needs special consideration (Another function applies)
+        elif source in special_sources:
+            continue
+        else:
+            unique_id=False
+        # ----------
+
+        aup.log("--"*40)
+        aup.log(f"--- Starting nodes proximity to pois using speed {walking_speed}km/hr for source {i}/{k}: {source}. ")
+
+        # 1.1a) Read pois from pois dir
+        aup.log(f"--- Reading pois dir.")
+        # Directory where pois to be examined are located
+        pois_dir = gral_dir + f'{source}.gpkg'
+        # Load all pois from directory
+        pois = gpd.read_file(pois_dir)
+
+        # ----------
+        # UNIQUE ID CONSIDERATION
+        if unique_id:
+            # Keep already existing unique ID and geometry
+            pois = pois[['ID','geometry']]
+        else:
+            # Set ID col as source name and keep geometry
+            pois['ID'] = source
+            pois = pois[['ID','geometry']]
+        # ----------
+
+        # Format
+        if pois.crs:
+            pois = pois.to_crs("EPSG:4326")
+        else:
+            pois = pois.set_crs("EPSG:4326")
+
+        # 1.1b) Clip pois to aoi
+        source_pois = gpd.sjoin(pois, aoi)
+        source_pois = source_pois[['ID','geometry']]
+        aup.log(f"--- Keeping {len(source_pois)} pois inside aoi from original {len(pois)} pois.")
+
+        if save_space:
+            del pois
+
+        # 1.1c) Calculate nodes proximity (Function pois_time())
+        aup.log(f"--- Calculating nodes proximity.")
+
+        # ----------
+        # UNIQUE ID CONSIDERATION
+        if unique_id:
+            # Function id_pois_time() consideres the unique ID belonging to each geometry of interest (goi).
+            source_nodes_time = aup.id_pois_time(G, nodes, edges, source_pois, source, 'length', walking_speed, 
+                                                 goi_id='ID', count_pois=count_pois, projected_crs=projected_crs)
+        else:
+            # Function pois_time() calculates proximity data from nodes to source (all) without considering any unique ID.
+            source_nodes_time = aup.pois_time(G, nodes, edges, source_pois, source,'length',walking_speed, 
+                                              count_pois, projected_crs)
+        # ----------
+
+        # 1.1d) Nodes_analysis format
+        source_nodes_time.rename(columns={'time_'+source:source},inplace=True)
+        nodes_analysis = source_nodes_time.copy()
+
+        if save_space:
+            del source_pois
+            del source_nodes_time
+
+        # if count_pois, include generated col
+        if count_pois[0]:
+            column_order = ['osmid'] + [source, f'{source}_{count_pois[1]}min'] + ['x','y','geometry']
+        else:
+            column_order = ['osmid'] + [source] + ['x','y','geometry']
+        nodes_analysis = nodes_analysis[column_order]
+
+        # 1.1e) Tidy data format (Allows loop-upload)
+        aup.log(f"--- Reordering datased as tidy data format.")
+        # Add source column to be able to extract source proximity data. Fill with current source.
+        nodes_analysis['source'] = source
+        # Rename source-specific column names as name that apply to all sources (source_time, source_15min)
+        nodes_analysis.rename(columns={source:'source_time'},inplace=True)
+        if count_pois[0]:
+            nodes_analysis.rename(columns={f'{source}_{count_pois[1]}min':f'source_{count_pois[1]}min'},inplace=True)
+        # Set column order
+        if count_pois[0]:
+            nodes_analysis = nodes_analysis[['osmid','source','source_time',f'source_{count_pois[1]}min','x','y','geometry']]
+        else:
+            nodes_analysis = nodes_analysis[['osmid','source','source_time','x','y','geometry']]
+        # Add city data
+        nodes_analysis['city'] = city
+        nodes_analysis[f'source_{count_pois[1]}min'] = nodes_analysis[f'source_{count_pois[1]}min'].astype(int)
+        
+        # 1.1f) Save output
+        aup.log(f"--- Saving nodes proximity to {source}.")
+        if save:
+            aup.gdf_to_db_slow(nodes_analysis, nodes_save_table, save_schema, if_exists='append')
+            aup.log(f"--- Saved nodes proximity to {source} in database.")
+
+        if local_save:
+            nodes_analysis.to_file(nodes_local_save_dir, driver='GPKG')
+            aup.log(f"--- Saved nodes proximity to {source} locally.")
+            
+        if save_space:
+            del nodes_analysis
+
+        i+=1
 
 if __name__ == "__main__":
     aup.log('--'*50)
@@ -144,6 +254,10 @@ if __name__ == "__main__":
     # Unique ID for each of them is 'ID'.
     special_sources = ['ep_plaza_small']
 
+    # --------------- POIS LOCAL DIR
+    # general pois local dir
+    gral_dir = '../data/external/temporal_fromjupyter/santiago/pois/'
+
     # --------------- AREA OF INTEREST
     # Area of interest (aoi)
     aoi_schema = 'projects_research'
@@ -182,6 +296,7 @@ if __name__ == "__main__":
     # --------------- SAVING DATA
     ##### WARNING ##### WARNING ##### WARNING #####
     save = True # save output to database?
+    project_code = 'p00' #Code to be added to saved files
     save_schema = 'projects_research'
     local_save = False # save output to local? (Make sure nodes_local_save_dir directory exists)
     ##### WARNING ##### WARNING ##### WARNING #####
@@ -205,15 +320,15 @@ if __name__ == "__main__":
         aup.log("--- Converting local data to OSMnx format network.")
         G, nodes, edges = create_filtered_navigable_network(public_space_quality_dir, projected_crs, filtering_column, filtering_value)
 
-
+    # Nodes proximity
     for walking_speed in walking_speed_list:
         aup.log('--'*45)
         aup.log(f"--- Running Script [Modified] for ep_plaza_small for speed = {walking_speed}km/hr.")
         str_walk_speed = str(walking_speed).replace('.','_')
 
         # Set saving dir and database table names
-        nodes_local_save_dir = f"../data/processed/santiago/santiago_nodesproximity_{str_walk_speed}.gpkg"
-        nodes_save_table = f'santiago_nodesproximity_{str_walk_speed}_kmh'
+        nodes_local_save_dir = f"../data/processed/santiago/santiago_{project_code}_nodesproximity_{str_walk_speed}.gpkg"
+        nodes_save_table = f'santiago_{project_code}_nodesproximity_{str_walk_speed}_kmh'
 
         if save:
             # Saved sources check (prevents us from uploading same source twice/errors on source list)
@@ -234,4 +349,4 @@ if __name__ == "__main__":
             
         # If passed source check, proceed to main function
         aup.log(f"--- Running Script for verified sources.")
-        main(source_list, aoi, nodes, edges, G, walking_speed, local_save, save)
+        main(source_list, aoi, G, nodes, edges, walking_speed, local_save, save)
