4ead8366a7a67abf02fc18b53246b1875b33ea70
Alex Loya <a.loyamr@gmail.com>
2024-07-31 18:50:46 +0000

    compiler_hqsl_wip02

--- aup/analysis.py
+++ aup/analysis.py
@@ -348,7 +348,7 @@ def walk_speed(edges_elevation):
 	return edges_speed
 
 
-def create_network(nodes, edges):
+def create_network(nodes, edges, projected_crs="EPSG:6372"):
 
 	"""
 	Create a network based on nodes and edges without unique ids and to - from attributes.
@@ -356,6 +356,7 @@ def create_network(nodes, edges):
 	Arguments:
 		nodes (geopandas.GeoDataFrame): GeoDataFrame with nodes for network in EPSG:4326
 		edges (geopandas.GeoDataFrame): GeoDataFrame with edges for network in EPSG:4326
+		projected_crs (str, optional): string containing projected crs to be used depending on area of interest. Defaults to "EPSG:6372".
 
 	Returns:
 		geopandas.GeoDataFrame: nodes GeoDataFrame with unique ids based on coordinates named osmid in EPSG:4326
@@ -368,11 +369,11 @@ def create_network(nodes, edges):
 
 	#Create unique ids for nodes and edges
 	##Change coordinate system to meters for unique ids
-	nodes = nodes.to_crs("EPSG:6372")
-	edges = edges.to_crs("EPSG:6372")
+	nodes = nodes.to_crs(projected_crs)
+	edges = edges.to_crs(projected_crs)
 
 	##Unique id for nodes based on coordinates
-	nodes['osmid'] = abs((nodes.geometry.x).astype(int)).astype(str)+abs((nodes.geometry.y).astype(int)).astype(str)
+	nodes['osmid'] = ((nodes.geometry.x).astype(int)).astype(str)+((nodes.geometry.y).astype(int)).astype(str)
 
 	##Set columns in edges for to [u] from[v] columns
 	edges['u'] = np.nan
@@ -383,8 +384,8 @@ def create_network(nodes, edges):
 	##Extract start and end coordinates for [u,v] columns
 	for index, row in edges.iterrows():
 		
-		edges.at[index,'u'] = str(int(abs(list(row.geometry.coords)[0][0])))+str(int(abs(list(row.geometry.coords)[0][1])))
-		edges.at[index,'v'] = str(int(abs(list(row.geometry.coords)[-1][0])))+str(int(abs(list(row.geometry.coords)[-1][1])))
+		edges.at[index,'u'] = str(int(list(row.geometry.coords)[0][0]))+str(int(list(row.geometry.coords)[0][1]))
+		edges.at[index,'v'] = str(int(list(row.geometry.coords)[-1][0]))+str(int(list(row.geometry.coords)[-1][1]))
 
 	#Add key column for compatibility with osmnx
 	edges['key'] = 0
@@ -393,7 +394,7 @@ def create_network(nodes, edges):
 	edges['u'] = edges.u.astype(int)
 	edges['v'] = edges.v.astype(int)
 	#Calculate edges lentgh
-	edges['length'] = edges.to_crs("EPSG:6372").length
+	edges['length'] = edges.to_crs(projected_crs).length
 	
 	#Change osmid to integer
 	nodes['osmid'] = nodes.osmid.astype(int)
