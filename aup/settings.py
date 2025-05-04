b2f98cfa295b22ab1c16340ac0c099cd08ccb0da
SaraRiveraM <a01068365@tec.mx>
2025-05-04 21:16:00 +0000

    Cambios realizadis en mi semana de incapacidad

--- aup/settings.py
+++ aup/settings.py
@@ -5,7 +5,6 @@ script_directory = Path(__file__).resolve().parent
 
 load_dotenv(script_directory /'.env')
 
-#load_dotenv('../../../')
 
 """Global settings, can be configured by user with utils.config()."""
 
