5526c43fafac8f05c49bcb7e8ea26f183ed03654
Alex Loya <a.loyamr@gmail.com>
2025-10-16 18:04:44 +0000

    Script 20 preparation to fix Culiacan

--- scripts/20-mun_gdf_to_hex.py
+++ scripts/20-mun_gdf_to_hex.py
@@ -12,8 +12,7 @@ if module_path not in sys.path:
 def main(city, res, mun_gdf, ageb_gdf, save=False):
 
     #############################################
-
-    # assertion errors
+    # ------------------------------ ASSERTION ERRORS ------------------------------
     # dissolve mun_gdf and ageb_gdf
     mun_buff = mun_gdf.to_crs("EPSG:6372").buffer(500)
     mun_buff = mun_buff.to_crs("EPSG:4326")
@@ -27,12 +26,10 @@ def main(city, res, mun_gdf, ageb_gdf, save=False):
     gdf_merge = pd.concat([ageb_buff, mun_buff])
     gdf_merge = gdf_merge.dissolve()
 
-
     #############################################
 
     # create hexagons grid
     hex_gdf = aup.create_hexgrid(gdf_merge, res)
-
     aup.log(f'Created {len(hex_gdf)} hexagons for {city}')
 
     # hexagons to municipality
@@ -44,9 +41,9 @@ def main(city, res, mun_gdf, ageb_gdf, save=False):
     cnt_join = hex_cnt.sjoin(mun_gdf).drop(columns='index_right')
 
     hex_merge = hex_gdf.merge(cnt_join[[f'hex_id_{res}','CVEGEO','NOMGEO','city']],
-             on=f'hex_id_{res}', how='outer')
-    aup.log(f'Merged hexagons with municipalities for {city}')
-    aup.log(f'Total hex_gdf: {len(hex_gdf)} and hex_merge: {len(hex_merge)}')
+                              on=f'hex_id_{res}', how='outer')
+    aup.log(f'Merged hexagons with municipalities for {city}.')
+    aup.log(f'Total hex_gdf: {len(hex_gdf)} and hex_merge: {len(hex_merge)}.')
 
     mun_hex_intersect = len(hex_merge.loc[hex_merge.CVEGEO.notna()])
 
@@ -54,15 +51,14 @@ def main(city, res, mun_gdf, ageb_gdf, save=False):
 
     # hexagons to ageb - define urban/rural
     ageb_join = ageb_gdf.sjoin(hex_merge).drop(columns='index_right')
-
-    pop_join = ageb_gdf.loc[ageb_gdf.cve_geo.isin(list(ageb_join.cve_geo.unique()))].pobtot.sum()
+    pop_join = ageb_gdf.loc[ageb_gdf.cvegeo_ageb.isin(list(ageb_join.cvegeo_ageb.unique()))].pobtot.sum()
     pop_ageb = ageb_gdf.pobtot.sum()
-    aup.log(f'Total ageb population: {pop_ageb} and joined ageb population: {pop_join}')
+    aup.log(f'Total ageb population: {pop_ageb} and joined ageb population: {pop_join}.')
     
     assert pop_ageb == pop_join, 'Population does not match'
 
     total_ageb = ageb_gdf.shape[0]
-    total_join = ageb_gdf.loc[ageb_gdf.cve_geo.isin(list(ageb_join.cve_geo.unique()))].shape[0]
+    total_join = ageb_gdf.loc[ageb_gdf.cvegeo_ageb.isin(list(ageb_join.cvegeo_ageb.unique()))].shape[0]
     aup.log(f'Total ageb: {total_ageb} and joined ageb: {total_join}')
 
     assert total_ageb == total_join, 'ageb does not match'
@@ -75,15 +71,14 @@ def main(city, res, mun_gdf, ageb_gdf, save=False):
     # fill missing data
     # fill CVEGEO, city and NOMGEO
     aup.log(f'Filling missing data for {city}')
-    ageb_join.loc[:,'CVEGEO'] = ageb_join.cve_geo.str[:5]
+    ageb_join.loc[:,'CVEGEO'] = ageb_join.cvegeo_ageb.str[:5]
     ageb_join.loc[:,'city'] = city
     ageb_join = ageb_join.drop(columns=['NOMGEO'])
     ageb_join = ageb_join.merge(mun_gdf[['CVEGEO','NOMGEO']], on='CVEGEO')
 
     # drop hex_id duplicates, preparing to join
     ageb_to_merge = ageb_join.drop_duplicates(subset=f'hex_id_{res}')
-
-    aup.log('Dropped duplicates')
+    aup.log('Dropped duplicates.')
 
     # update missing data
     left_a = hex_merge.set_index(f'hex_id_{res}')
@@ -92,15 +87,14 @@ def main(city, res, mun_gdf, ageb_gdf, save=False):
     hex_fill = left_a.reindex(columns=left_a.columns.union(right_a.columns))
     hex_fill.update(right_a)
     hex_fill.reset_index(inplace=True)
-
-    aup.log('Updated missing data')
+    aup.log('Updated missing data.')
 
     hex_fill = hex_fill.loc[~((hex_fill.CVEGEO.isna())&(hex_fill['type']=='rural'))] 
 
     urban_len = len(hex_fill.loc[hex_fill['type'] == 'urban'])
     rural_len = len(hex_fill.loc[hex_fill['type'] == 'rural'])
-    aup.log(f'Created {urban_len} urban and {rural_len} rural hexagons')
-    aup.log(f'Final hex_fill length: {len(hex_fill)} compaered to intersection: {mun_hex_intersect} length')
+    aup.log(f'Created {urban_len} urban and {rural_len} rural hexagons.')
+    aup.log(f'Final hex_fill length: {len(hex_fill)} compaered to intersection: {mun_hex_intersect} length.')
 
     if save:
         schema = 'hexgrid'
@@ -121,49 +115,52 @@ if __name__ == "__main__":
     aup.log('--'*20)
     aup.log('Starting script')
 
+    # ------------------------------ SCRIPT CONFIGURATION - ANALYSIS ------------------------------
+    # Define hexagon resolutions to process
     res_list = [r for r in range(8,12)]
 
+    # Overwrite res list for data fix
+    res_list = [11]
+
+    # ------------------------------ SCRIPT CONFIGURATION - AREA OF INTEREST ------------------------------
+    # Get all available city names
     schema = 'metropolis'
     table = 'metro_gdf_2020'
-
     query = f"SELECT DISTINCT city from {schema}.{table}"
-
     city_names = aup.df_from_query(query)
-
     aup.log(f"Downloaded city names for {len(city_names)} cities")
 
-    save = True
+    # Overwrite city names for data fix
+    city_names = ['Culiacan']
 
-    # for city in city_names.city.unique():
+    # ------------------------------ SCRIPT CONFIGURATION - SAVING ------------------------------
+    save = True #------ Set True if saving to database
+
+    # ------------------------------ SCRIPT START ------------------------------
     for city in city_names:
-        # download municipality polygons
+        # Download municipality polygons
         schema = 'metropolis'
         table = 'metro_gdf_2020'
-
         query = f"SELECT * FROM {schema}.{table} WHERE \"city\" = \'{city}\'"
-
         mun_gdf = aup.gdf_from_query(query)
-
         aup.log(f'Downloaded {len(mun_gdf)} municipalities for {city}')
 
-        # download ageb polygons
-
-        schema = 'censoageb'
-        table = 'censoageb_2020'
-
+        # Download ageb polygons
+        schema = 'sociodemografico'
+        table = 'censo_inegi_20_ageb'
         ageb_gdf = gpd.GeoDataFrame()
-
         for cvegeo in mun_gdf.CVEGEO.unique():
-            query = f"SELECT cve_geo,pobtot,geometry FROM {schema}.{table} WHERE \"cve_geo\" LIKE \'{cvegeo}%%\'"
+            query = f"SELECT cvegeo_ageb,pobtot,geometry FROM {schema}.{table} WHERE \"cvegeo_ageb\" LIKE \'{cvegeo}%%\'"
             ageb_gdf = pd.concat([ageb_gdf, aup.gdf_from_query(query)],
-                                ignore_index = True, axis = 0)
-            
-        aup.log(f'Downloaded {len(ageb_gdf)} ageb features for {city} with {ageb_gdf.pobtot.sum()} persons')
+                                 ignore_index = True, axis = 0
+                                 )
+        aup.log(f'Downloaded {len(ageb_gdf)} ageb features for {city} with {ageb_gdf.pobtot.sum()} persons.')
 
+        # Process each resolution
         for r in res_list:
-            aup.log(f'\n Processing {city} with resolution {r}')
+            aup.log(f'\n Processing {city} with resolution {r}.')
             try:
                 main(city, r, mun_gdf, ageb_gdf, save=save)
             except:
-                aup.log(f'Assertion error processing {city} with resolution {r}')
+                aup.log(f'Assertion error processing {city} with resolution {r}.')
                 continue
